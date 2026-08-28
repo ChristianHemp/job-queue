00558c14761890ef60a37a670c7619c036a4df9e
Christian Hemprich <christian@hempasaur.lan>
2026-08-27 19:36:22 -0700

    Refactored job execution system

--- app/main.py
+++ app/main.py
@@ -41,6 +41,7 @@ class Job(BaseModel):
     result: Any | None = None
     error: str | None = None
 
+
 jobs: dict[int, Job] = {}
 next_id = 1
 
@@ -48,56 +49,59 @@ def execute_job(job: Job) -> Job:
     job.status = JobStatus.RUNNING
     job.result = None
     job.error = None
-    job_type = job.job_type
+    job_exec_function = JOB_REGISTRY.get(job.job_type)
 
-    if job_type == JobType.SUM_NUMBERS:
-        try:
-            assert isinstance(job.payload, SumNumbersPayload)
-            numbers = job.payload.numbers
-            total = 0
-
-            for num in numbers:
-                total += num
-
-            job.status = JobStatus.COMPLETED
-            job.result = total
-            return job
-        except Exception as e:
-            job.status = JobStatus.FAILED
-            job.error = str(e)
-            return job
-    elif job_type == JobType.PROCESS_CSV:
-        try:
-            assert isinstance(job.payload, CsvPayload)
-            path = job.payload.file_path
-            column = job.payload.column
-
-            job.status = JobStatus.COMPLETED
-            job.result = path + str(column)  # placeholder work implement csv processing later
-            return job
-        except Exception as e:
-            job.status = JobStatus.FAILED
-            job.error = str(e)
-            return job
-
-    else:
+    if job_exec_function is None:
         job.status = JobStatus.FAILED
         job.error = "Unsupported Job Type"
+        return job
+
+    try:
+        job.result = job_exec_function(job)
+        job.status = JobStatus.COMPLETED
+    except Exception as e:
+        job.error = str(e)
+        job.status = JobStatus.FAILED
 
     return job
+    
+def execute_sum_numbers(job: Job) -> int:
+    assert isinstance(job.payload, SumNumbersPayload)
+    numbers = job.payload.numbers
+
+    return sum(numbers)
+
+def execute_process_csv(job: Job) -> str:
+    assert isinstance(job.payload, CsvPayload)
+    path = job.payload.file_path
+    column = job.payload.column
+
+    return path + str(column) # placeholder work implement csv processing later
+
+JOB_REGISTRY = {
+    JobType.SUM_NUMBERS: execute_sum_numbers,
+    JobType.PROCESS_CSV: execute_process_csv
+}
 
 @app.get("/")
 def root():
     return {"message": "server is running"}
 
 @app.post("/jobs/{job_id}/run")
-def run(job_id: int) -> Job:
+def run_job(job_id: int) -> Job:
     if job_id not in jobs:
         raise HTTPException(status_code=404, detail="Job Not Found")
 
-    execute_job(jobs[job_id])
+    job = jobs[job_id]
 
-    return jobs[job_id]
+    if job.status == JobStatus.RUNNING:
+        raise HTTPException(status_code=409, detail="Job already running")
+
+    if job.status == JobStatus.COMPLETED:
+        raise HTTPException(status_code=409, detail="Job already completed")
+
+    execute_job(job)
+    return job
     
 
 @app.post("/jobs")
@@ -124,5 +128,5 @@ def get_job(job_id: int) -> Job:
     return jobs[job_id]
 
 @app.get("/jobs")
-def get_jobs() -> dict:
+def get_jobs() -> dict[int, Job]:
     return jobs

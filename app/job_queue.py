d6bff6ffe0e40717b549a0a61fc8618daba777b1
Christian Hemprich <christian@hemphemphemp.lan>
2026-08-29 12:32:32 -0700

    Add design rationale comments

--- app/job_queue.py
+++ app/job_queue.py
@@ -5,6 +5,7 @@ job_queue = PriorityQueue()
 counter = count()
 
 def enqueue(job_id: int, job_priority: int) -> None:
+    # Counter number allows priority queue to keep FIFO ordering among equal priority jobs
     job_queue.put((job_priority, next(counter), job_id))
 
 def dequeue() -> int:
